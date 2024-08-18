50602cfd620d6bbb4b7583bdec87a72010fa1bf1
AnasE2006 <[mn242.ae@gmail.com]>
2024-08-18 15:03:34 -0500

    Write operate function to handle inputs

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ function divide(num1, num2) {
 function modulo(num1, num2) {
     return num1 % num2;
 }
+
+let input1 = NaN;
+let input2 = NaN;
+let operation = NaN;
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            return add(num1,num2);
+        case "-":
+            return subtract(num1,num2);
+        case "*":
+            return multiply(num1,num2);
+        case "/":
+            return divide(num1,num2);
+        case "%":
+            return modulo(num1,num2);
+    }
+}
